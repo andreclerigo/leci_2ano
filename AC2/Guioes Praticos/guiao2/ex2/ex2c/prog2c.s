5f83f209c8d20ee87655c7c2bcfec4805b33fa02
andreclerigo <andreclerigo@outlook.com>
2021-03-24 14:44:58 +0000

    ex2

---
+++ AC2/Guioes Praticos/guiao2/ex2/ex2c/prog2c.s
@@ -0,0 +1,46 @@
+        .equ    RESET_CORE_TIMER, 12
+        .equ    READ_CORE_TIMER, 11
+        .equ putChar, 3
+        .equ printInt, 6
+        .data
+str1:   .asciiz "Hello World\n"
+        .text
+        .globl main
+
+main:                                   # int main(void) {
+        addiu   $sp, $sp, -8            #       abrir espaco na pilha
+        sw      $ra, 0($sp)             #       guardar o $ra
+        sw      $s0, 4($sp)             #       guardar o $s0
+        li      $s0, 0                  #       counter = 0;
+while:                                  #       while(1) {
+        li      $a0, 1000               #               $a0 = 1000;
+        jal     delay                   #               delay(1000);
+
+        addi    $s0, $s0, 1             #
+        move    $a0, $s0                #               $a0 = ++counter;
+        li      $a1, 0x0004000A         #               $a1 = 10 | 4 << 16
+        li      $v0, printInt           #               $v0 = 6;
+        syscall                         #               printInt(++counter, 10 | 4 << 16);
+        li      $a0, '\r'               #               $a0 = '\r'
+        li      $v0, putChar            #               $v0 = 3;
+        syscall                         #               putChar('\r');
+        j       while                   #       }
+        li      $v0, 0                  #       return 0;
+        lw      $ra, 0($sp)             #       report o $ra
+        lw      $s0, 4($sp)             #       repor o $s0
+        addiu   $sp, $sp, 8             #       repor espaco da pilha
+        jr      $ra                     # }
+
+
+delay:                                  # void delay(int ms) {
+        move    $t0, $a0                #       $t0 = ms;
+for:                                    #       for(; ms > 0; ms--) {
+        li      $v0, RESET_CORE_TIMER   #               
+        syscall                         #               resetCoreTimer();
+        li      $v0, READ_CORE_TIMER    #
+        syscall                         #               readCoreTimer();
+        blt     $v0, 20000, for         #               while(readCoreTimer() < K);
+        addi    $t0, $t0, -1            #               ms--;
+        j       for                     #       }
+endfor:                                 #       
+        jr      $ra                     # }
