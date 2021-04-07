9bd22764f025e4d99cd416134ed5a8f74b51980b
AndreClerigo Laptop <andreclerigo@outlook.com>
2021-04-07 14:34:27 +0100

    ex1 fix

--- AC2/Guioes Praticos/guiao3/parte1/ex1/ex1.s
+++ AC2/Guioes Praticos/guiao3/parte1/ex1/ex1.s
@@ -1,25 +1,31 @@
 	.equ SFR_BASE_HI, 0xBF88
 	.equ TRISE, 0x6100
+	.equ PORTE, 0x6110
 	.equ LATE, 0x6120
 	.equ TRISB, 0x6040
-	.equ PORTB, 0x6060
+	.equ PORTB, 0x6050
+	.equ LATB, 0x6060
 	.data
 	.text
 	.globl main
 
 main:					# int main(void) {
 	lui	$t0, SFR_BASE_HI	#
-	lw	$t1, TRISE($t0)		#	$t1 = 0xBF886100 = TRISE
+	lw	$t1, TRISE($t0)		#	$t1 = [TRISE]
 	andi	$t1, $t1, 0xFFFE	#	RE0 = 0 (OUTPUT)
 	sw	$t1, TRISE($t0)		#	WRITE TRISE register
 					#
-	lw	$t2, TRISB($t0)		#	$t1 = 0xBF886100 = TRISB
-	ori	$t2, $t2, 0x1		#	RB0 = 1 (INPUT)
-	sw	$t2, TRISB($t0)		#	WRITE TRISB register
+	lw	$t1, TRISB($t0)		#	$t1 = [TRISB]
+	ori	$t1, $t1, 0x1		#	RB0 = 1 (INPUT)
+	sw	$t1, TRISB($t0)		#	WRITE TRISB register
 
 loop:					#	while(true) {
-	lw	$t3, PORTB($t0)		#		$t3 = READ (Read PORTB register)
-	andi	$t3, $t3, 1		#		$t3 = PORTB0
-	sw	$t3, LATE($t0)		#		WRITE LATE register with PORTB0
+	lw	$t1, PORTB($t0)		#		$t1 = [PORTB]
+	andi	$t1, $t1, 1		#		$t1 = RB0
+	lw	$t2, LATE($t0)		#		$t2 = [LATE]
+	andi	$t2, $t2, 0xFFFE	#		RE0 = 0
+	or	$t2, $t2, $t1		#		RE0 = REB0
+	sw	$t2, LATE($t0)		#
 	j	loop			#	}
+	li 	$v0, 0			#	return 0;
 	jr	$ra 			# }
