e1b7c749ed27aaa872dc7f62ccb3a06bb746e8fc
AndreClerigo Laptop <andreclerigo@outlook.com>
2021-04-07 14:54:31 +0100

    ex3 parte1

--- AC2/Guioes Praticos/guiao3/parte1/ex3/ex3.s
+++ AC2/Guioes Praticos/guiao3/parte1/ex3/ex3.s
@@ -12,21 +12,21 @@
 main:					# int main(void) {
 	lui	$t0, SFR_BASE_HI	#
 	lw	$t1, TRISE($t0)		#	$t1 = [TRISE]
-	andi	$t1, $t1, 0xFFFE	#	RE0 = 0 (OUTPUT)
+	andi	$t1, $t1, 0xFFF0	#	RE0, RE1, RE2, RE3 = 0 (OUTPUT)
 	sw	$t1, TRISE($t0)		#	WRITE TRISE register
 					#
 	lw	$t1, TRISB($t0)		#	$t1 = [TRISB]
-	ori	$t1, $t1, 0x1		#	RB0 = 1 (INPUT)
+	ori	$t1, $t1, 0x000F	#	RB0, RB1, RB2, RB3 = 1 (INPUT)
 	sw	$t1, TRISB($t0)		#	WRITE TRISB register
 
 loop:					#	while(true) {
 	lw	$t1, PORTB($t0)		#		$t1 = [PORTB]
-	andi	$t1, $t1, 1		#		$t1 = RB0
-	xori    $t1, $t1, 0x0001	#		RB0 = RB0\
+	andi	$t1, $t1, 0x0000F	#		$t1 = RB3, RB2, RB1, RB0
+	xori    $t1, $t1, 0x00009	#		RB0 = RB0\ && RB3 = RB3\
 	lw	$t2, LATE($t0)		#		$t2 = [LATE]
-	andi	$t2, $t2, 0xFFFE	#		RE0 = 0
-	or	$t2, $t2, $t1		#		RE0 = RB0\
-	sw	$t2, LATE($t0)		#
+	andi	$t2, $t2, 0xFFF0	#		RE3, RB2, RB1, RB0 = 0
+	or	$t2, $t2, $t1		#		RE3 = RB3\, RE2 = RB2, RE1 = RB1, RE0 = RB0\
+	sw	$t2, LATE($t0)		#               
 	j	loop			#	}
 	li 	$v0, 0			#	return 0;
 	jr	$ra 			# }
