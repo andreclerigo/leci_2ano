c1d5520d6cb27073481849d3d5146b757b5e8fe2
AndreClerigo Laptop <andreclerigo@outlook.com>
2021-04-14 16:15:50 +0100

    ex7 guiao4

---
+++ AC2/Guioes Praticos/guiao4/parte1/ex7/ex7.c
@@ -0,0 +1,25 @@
+#include <detpic32.h>
+
+int display7Scode[] = {
+                        0x3F, //0
+                        0x06, //1
+                        0x5B, //2
+                        0x4F, //3
+                        0x66, //4
+                        0x6D, //5
+                        0x7D, //6
+                        0x07, //7
+                        0x7F, //8
+                        0x6F, //9
+                        0x77, //A
+                        0x7C, //b
+                        0x39, //C
+                        0x57, //d
+                        0x79, //E
+                        0x71  //F
+                    };
+
+int main (void)
+{
+    return 0;
+}
