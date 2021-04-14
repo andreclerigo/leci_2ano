0b6c4de5c103fd18e3cf1764d5a2ac04fca68519
AndreClerigo Laptop <andreclerigo@outlook.com>
2021-04-14 15:43:06 +0100

    ex4 guiao 4

---
+++ AC2/Guioes Praticos/guiao4/parte1/ex4/ex4.c
@@ -0,0 +1,38 @@
+#include <detpic32.h>
+
+void delay(int ms);
+
+int main (void)
+{
+    unsigned char segment;
+    int i;
+    LATDbits.LATD6 = 1;     // Display h off
+    LATDbits.LATD5 = 0;     // Display H on
+    TRISB = TRISB & 0x80FF; // RB14 - RB8 as output
+    TRISD = TRISD & 0xFF9F; // RD6 - RD5 as output
+
+    while (1)
+    {   
+        LATDbits.LATD6 = !LATDbits.LATD6;
+        LATDbits.LATD5 = !LATDbits.LATD5;
+        segment = 1;
+        for (i = 0; i < 7; i++)
+        {
+            LATB = (LATB & 0x80FF) | ((unsigned int)(segment)) << 8;
+            delay(500);     // wait 0.5s
+            segment = segment << 1;
+        }
+    }
+    
+    return 0;
+}
+
+void delay(int ms)
+{
+    for (;ms > 0; ms--)
+    {
+        resetCoreTimer();
+        readCoreTimer();
+        while(readCoreTimer() < 20000);
+    }
+}
