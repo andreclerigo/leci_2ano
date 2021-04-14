e3f14d1214487049f863499106f5a6106701de04
AndreClerigo Laptop <andreclerigo@outlook.com>
2021-04-14 15:56:41 +0100

    ex6 guiao 4

---
+++ AC2/Guioes Praticos/guiao4/parte1/ex6/ex6_50/ex6_50.c
@@ -0,0 +1,37 @@
+#include <detpic32.h>
+
+void delay(int ms);
+
+int main (void)
+{
+    unsigned char segment;
+    int i;
+    LATD = (LATD & 0xFF9F) | 0x0040;    // display high active, low inactive
+    TRISB = TRISB & 0x80FF; // RB14 - RB8 as output
+    TRISD = TRISD & 0xFF9F; // RD6 - RD5 as output
+
+    while (1)
+    {   
+        LATD = LATD ^ 0x0060;               // toggle display selection
+
+        segment = 1;
+        for (i = 0; i < 7; i++)
+        {
+            LATB = (LATB & 0x80FF) | ((unsigned int)(segment)) << 8;
+            delay(20);                  // freq = 50hz
+            segment = segment << 1;
+        }
+    }
+    
+    return 0;
+}
+
+void delay(int ms)
+{
+    for (;ms > 0; ms--)
+    {
+        resetCoreTimer();
+        readCoreTimer();
+        while(readCoreTimer() < 20000);
+    }
+}
