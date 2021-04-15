56dc893f82350e854a389fc7ed073a4f3b7a9084
AndreClerigo Laptop <andreclerigo@outlook.com>
2021-04-15 10:48:49 +0100

    ex9 guiao4

---
+++ AC2/Guioes Praticos/guiao4/parte1/ex9/ex9.c
@@ -0,0 +1,41 @@
+#include <detpic32.h>
+
+void delay(int ms);
+
+int main (void)
+{
+    static const char display7Scode[] = {
+                                        0x3F, //0
+                                        0x06, //1
+                                        0x5B, //2
+                                        0x4F, //3
+                                        0x66, //4
+                                        0x6D, //5
+                                        0x7D, //6
+                                        0x07, //7
+                                        0x7F, //8
+                                        0x6F, //9
+                                        0x77, //A
+                                        0x7C, //b
+                                        0x39, //C
+                                        0x5E, //d
+                                        0x79, //E
+                                        0x71  //F
+                                    };
+
+    TRISB = TRISB | 0x000F;         // RB3 to RB0 as input
+    TRISB = TRISB & 0x80FF;         // RB14 to RB8 as output
+    TRISD = TRISD & 0xFF9F;
+    LATDbits.LATD6 = 1;             // Display high active
+    int index;
+    int value;
+
+    while (1)
+    {
+        index = PORTB & 0x000F;
+        value = display7Scode[index];
+        LATB = (LATB & 0x80FF) | ((unsigned int)(value)) << 8;
+    }
+    
+    return 0;
+}
