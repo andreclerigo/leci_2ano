b477ef4724a059ae13ac141bddf2f5597bf90dda
AndreClerigo Laptop <andreclerigo@outlook.com>
2021-04-21 16:43:50 +0100

    ex1 parte1 comments

--- AC2/Guioes Praticos/guiao4/parte1/ex1/ex1.c
+++ AC2/Guioes Praticos/guiao4/parte1/ex1/ex1.c
@@ -4,12 +4,12 @@ void delay(int ms);
 
 int main(void) 
 {
-    LATCbits.LATC14 = 0;
-    TRISCbits.TRISC14 = 0;
+    LATCbits.LATC14 = 0;        // Output value as 0
+    TRISCbits.TRISC14 = 0;      // Configure port as output
 
     while (1)
     {   
-        delay(500);
+        delay(500);             // freq = 2Hz
         LATCbits.LATC14 = !LATCbits.LATC14;
     }
     return 0;
