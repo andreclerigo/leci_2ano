8fb7f453fa8a8e05cb43e647c93599b9cb8983f7
AndreClerigo Laptop <andreclerigo@outlook.com>
2021-04-21 16:45:05 +0100

    ex2 parte1 comments

--- AC2/Guioes Praticos/guiao4/parte1/ex2/ex2.c
+++ AC2/Guioes Praticos/guiao4/parte1/ex2/ex2.c
@@ -4,16 +4,16 @@ void delay(int ms);
 
 int main (void)
 {
-    TRISE = TRISE & 0xFFF0;
+    TRISE = TRISE & 0xFFF0;     // Configure port as outpus
     int cnt = 0;
 
     while (1)
     {
         LATE = (LATE & 0xFFF0) | cnt;
-        delay(250);     // 250ms to get 4hz
+        delay(250);             // delay 250ms -> 4hz
         cnt++;
 
-        if(cnt > 15)
+        if(cnt > 15)            // only count to 15
         {
             cnt = 0;
         }
