4062ef65e7f01f0203aa078d1fa4c60e310e80b7
AndreClerigo Laptop <andreclerigo@outlook.com>
2021-05-02 14:07:19 +0100

    fix ex2adicional

--- AC2/Guioes Praticos/guiao1/ex2adicional/prog2ad.s
+++ AC2/Guioes Praticos/guiao1/ex2adicional/prog2ad.s
@@ -5,7 +5,9 @@
         .equ printInt10, 7
         .data
 s1:     .asciiz "Introduza 2 strings: "
-s2:     .asciiz "Resultados:\n"
+s2:     .asciiz "Resultados: "
+s3:		.asciiz "\n"
+s4:		.asciiz " "
 str1:   .space 21                       # char str1[STR_MAX_SIZE + 1]
 str2:   .space 21                       # char str2[STR_MAX_SIZE + 1]
 str3:   .space 41                       # char str3[2 * STR_MAX_SIZE + 1]
@@ -24,13 +26,21 @@ main:                                   # int main(void) {
         li      $a1, STR_MAX_SIZE       #       $a1 = STR_MAX_SIZE;
         li      $v0, readStr            #       $v0 = 9;
         syscall                         #       readStr( str1, STR_MAX_SIZE );
-                                        #
+                  	                    #
+		la		$a0, s4					#		$a0 = s4;
+		li		$v0, printStr			#		$v0 = 8;
+		syscall							#		printStr(' ');
+										#		
         la      $a0, str2               #       $a0 = str2;
         li      $a1, STR_MAX_SIZE       #       $a1 = STR_MAX_SIZE;
         li      $v0, readStr            #       $v0 = 9;
         syscall                         #       readStr( str2, STR_MAX_SIZE );
                                         #
-        la      $a0, s2                 #       $a0 = s1;
+		la		$a0, s3					#		$a0 = s3;
+		li		$v0, printStr			#		$v0 = 8;
+		syscall							#		printStr('\n');
+        								#
+		la      $a0, s2                 #       $a0 = s1;
         li      $v0, printStr           #       $v0 = 8;
         syscall                         #       printStr("Resultados:\n");
                                         #
@@ -41,6 +51,10 @@ main:                                   # int main(void) {
         li      $v0, printInt           #       $v0 = 6;
         syscall                         #       printInt(strlen(str1), 10);
                                         #
+		la		$a0, s4					#		$a0 = s4;
+		li		$v0, printStr			#		$v0 = 8;
+		syscall							#		printStr(' ');
+										#
         la      $a0, str2               #       $a0 = str1;
         jal     strlen                  #       strlen(str1);
         move    $a0, $v0                #       $a0 = return strlen(str1);
@@ -48,19 +62,25 @@ main:                                   # int main(void) {
         li      $v0, printInt           #       $v0 = 6;
         syscall                         #       printInt(strlen(str1), 10);
                                         #
+		la		$a0, s3					#		$a0 = s3;
+		li		$v0, printStr			#		$v0 = 8;
+		syscall							#		printStr('\n');
+										#
         la      $a0, str3               #       $a0 = str3;
         la      $a1, str1               #       $a1 = str1;
         jal     strcpy                  #       strcpy(str3, str1);	
-        
-        
         la      $a0, str3               #       $a0 = str3;
         la      $a1, str2               #       $a1 = str2;
         jal     strcat                  #       strcat(str3, str2);
  
         move    $a0, $v0                #       $v0 = 8;
         li      $v0, printStr           #       
-        syscall                         #	printStr(strcat(str3, str1));
+        syscall                         #		printStr(strcat(str3, str1));
 
+		la		$a0, s4					#		$a0 = s4;
+		li		$v0, printStr			#		$v0 = 8;
+		syscall							#		printStr(' ');
+										#
         la      $a0, str1               #       $a0 = str1;
         la      $a1, str2               #       $a1 = str2;
         jal     strcmp                  #       strcmp(str1, str2);
@@ -78,7 +98,7 @@ strlen:                                 # int strlen(char *s) {
         move    $t0, $a0                #       $t0 = &s
         li      $v0, 0                  #       len = 0;
 for:    lb      $t1, 0($t0)             #       $t1 = *s
-        beq     $t1, '\0', endfor       #       for(len = 0; *s != '\0'; len++, s++) {
+        beq     $t1, $0, endfor       	#       for(len = 0; *s != '\0'; len++, s++) {
         addiu   $t0, $t0, 1             #               $t0++;   //next position of string
         addi    $v0, $v0, 1             #               len++;
         j       for                     #       }
@@ -90,7 +110,7 @@ strcpy:                                 # char *strcpy(char *dst, char *src) {
         move    $v0, $a0                #       char *p = dst;
 for2:   lb      $t1, 0($a1)             #       $t1 = *src;
         sb      $t1, 0($a0)             #               *dst = *src;
-        beq     $t1, '\0', endfor2      #       for(; *src = '\0'; dst++, src++) { ###mbc troquei esta instrução 
+        beq     $t1, $0, endfor2      	#       for(; *src = '\0'; dst++, src++) { ###mbc troquei esta instrução 
         addiu   $a0, $a0, 1             #               dst++;
         addiu   $a1, $a1, 1             #               src++;
         j       for2                    #       }
@@ -105,7 +125,7 @@ strcat:                                 # char *strcat(char *dst, char *src) {
         move    $s0, $a0                #       $s0 = dst;
 
 for3:   lb      $t0, 0($a0)             #       for (; *dst != '\0'; dst++) {
-        beq     $t0, '\0', endfor3      #
+        beq     $t0, $0, endfor3      	#
         addiu   $a0, $a0, 1             #               dst++;
         j       for3                    #       }
 endfor3:                                #       
@@ -125,7 +145,7 @@ strcmp:                                 # int strcmp(char *s1, char *s2) {
 for4:   lb      $t0, 0($a0)             #       $t0 = *s1      
         lb      $t1, 0($a1)             #       $t1 = *s2
         bne     $t0, $t1, endfor4       #       for( ;(*s1 == *s2)  &&
-        beq     $t0, '\0', endfor4      #       (*s1 != '\0'); s1++, s2++) {
+        beq     $t0, $0, endfor4        #       (*s1 != '\0'); s1++, s2++) {
         addiu   $a0, $a0, 1             #               s1++;
         addiu   $a1, $a1, 1             #               s2++;
         j       for4                    #       }
