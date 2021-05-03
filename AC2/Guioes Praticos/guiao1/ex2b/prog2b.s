24aa1a31c4d561f64269b0a08910e4314a6d3d54
AndreClerigo Laptop <andreclerigo@outlook.com>
2021-05-04 00:08:49 +0100

    fix ex2b guiao1

--- AC2/Guioes Praticos/guiao1/ex2b/prog2b.s
+++ AC2/Guioes Praticos/guiao1/ex2b/prog2b.s
@@ -8,11 +8,11 @@ main:                               # int main(void){
 do:                                 #    do {
         li      $v0, getChar        #        c = getChar();
         syscall                     #        
-        move    $a0, $v0            #         $a0 = c;
-if:     beq     $v0, '\n', while    #        if(c!= '\n')
-        addiu   $a0, $a0, 1         #           c = c+1;
+        move    $t0, $v0            #        $a0 = c;
+if:     beq     $t0, '\n', while    #        if(c!= '\n')
+        addiu   $a0, $t0, 1         #           c = c+1;
         li      $v0, putChar        #           putChar(c+1);
         syscall                     #
-while:  bne     $a0, '\n', do       #    }while(c != '\n');
+while:  bne     $t0, '\n', do       #    }while(c != '\n');
         li      $v0, 0              #    return 0;
         jr      $ra                 # }  fim do programa
