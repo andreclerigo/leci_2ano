a82ece200075ebba3ca4cf169a60d1fbb68aca20
AndreClerigo Laptop <andreclerigo@outlook.com>
2021-05-26 13:59:20 +0100

    fix comentario 4b

--- AC2/Guioes/guiao7/parte1/ex4/ex4b/ex4b.c
+++ AC2/Guioes/guiao7/parte1/ex4/ex4b/ex4b.c
@@ -9,16 +9,16 @@ int main(void)
 
     T1CONbits.TCKPS = 3;    // 1:256 prescaler Fout_presc = 78125;
     PR1 = 39062;            // Fout = 20Mhz / 256(39062 + 1) = 1,9999Hz
-    TMR1 = 0;               // Reset timer T3 count register
-    T1CONbits.TON = 1;      // Enable timer T3   
+    TMR1 = 0;               // Reset timer T1 count register
+    T1CONbits.TON = 1;      // Enable timer T1 
 
     IPC3bits.T3IP = 2;      // Interrupt priority 2
     IEC0bits.T3IE = 1;      // Enable timer T3 interrupts
     IFS0bits.T3IF = 0;      // Reset timer T3 interrupt flag
     
     IPC1bits.T1IP = 1;      // Interrupt priority 1
-    IEC0bits.T1IE = 1;      // Enable timer T3 interrupts
-    IFS0bits.T1IF = 0;      // Reset timer T3 interrupt flag
+    IEC0bits.T1IE = 1;      // Enable timer T1 interrupts
+    IFS0bits.T1IF = 0;      // Reset timer T1 interrupt flag
     
     EnableInterrupts();
     while (1);    
