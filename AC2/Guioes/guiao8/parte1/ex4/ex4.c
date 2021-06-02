a3baea10e4c117594a4991f11161dd2c1e95663b
AndreClerigo Laptop <andreclerigo@outlook.com>
2021-06-02 15:37:08 +0100

    ex4&5 done

---
+++ AC2/Guioes/guiao8/parte1/ex4/ex4.c
@@ -0,0 +1,29 @@
+#include <detpic32.h>
+
+void configUart(unsigned int baud, char parity, unsigned int stopbits)
+{
+    // Configure UART2
+    if (baud >= 600 || baud <= 115200)
+        U2BRG = (((20*10^6) + 8 * baud) / (16 * baud)) - 1;    // Aproximate to closest baudrate
+    else
+        U2BRG = 10;                     // DEFAULT 115200 baudrate
+    
+    if (parity == 'N')
+        U2MODEbits.PDSEL = 0b00;        // 00 = 8-bit data, no parity
+    else if (parity == 'E')
+        U2MODEbits.PDSEL = 0b01;        // 01 = 8-bit data, even parity
+    else if (parity == 'O')
+        U2MODEbits.PDSEL = 0b10;        // 10 = 8-bit data, odd parity
+    else
+        U2MODEbits.PDSEL = 0b00;        // DEFAULT NO PARITY 8 BITS
+
+    if (stopbits == 1 || stopbits == 2)  
+        U2MODEbits.STSEL = stopbits - 1;// number of stopbits
+    else
+        U2MODEbits.STSEL = 0;           // DEFAULT 1 STOPBIT
+
+    U2MODEbits.BRGH = 0;                // Divide by 16
+    U2STAbits.URXEN = 1;                // Enable Receiver
+    U2STAbits.UTXEN = 1;                // Enable Transmitter
+    U2MODEbits.ON = 1;                  // Enable UART2
+}
---
+++ AC2/Guioes/guiao8/parte1/ex5/ex5.c
@@ -0,0 +1,71 @@
+#include <detpic32.h>
+
+void delay(int ms);
+void putc(char byte2send);
+void puts(char *str);
+void configUart(unsigned int baud, char party, unsigned int stopbits);
+
+int main(void)
+{
+    configUart(115200, 'N', 1);
+    
+    while (1)
+    {
+        puts("String de teste\n");
+        delay(1000);            // wait 1s
+    }
+    
+    return 0;
+}
+
+void puts(char *str)
+{
+    while (*str != '\0')
+    {
+        putc(*str);
+        str++;
+    }
+}
+
+void putc(char byte2send)
+{
+    while (U2STAbits.UTXBF == 1);
+    U2TXREG = byte2send;
+}
+
+void delay(int ms)
+{   
+    for (; ms > 0; ms--)
+    {
+        resetCoreTimer();
+        while (readCoreTimer() < 20000);
+    }
+}
+
+void configUart(unsigned int baud, char parity, unsigned int stopbits)
+{
+    // Configure UART2
+    if (baud >= 600 && baud <= 115200)
+        U2BRG = ((PBCLK + 8 * baud) / (16 * baud)) - 1;    // Aproximate to closest baudrate
+    else
+        U2BRG = 10;                     // DEFAULT 115200 baudrate
+    
+    if (parity == 'N')
+        U2MODEbits.PDSEL = 0b00;        // 00 = 8-bit data, no parity
+    else if (parity == 'E')
+        U2MODEbits.PDSEL = 0b01;        // 01 = 8-bit data, even parity
+    else if (parity == 'O')
+        U2MODEbits.PDSEL = 0b10;        // 10 = 8-bit data, odd parity
+    else
+        U2MODEbits.PDSEL = 0b00;        // DEFAULT NO PARITY 8 BITS
+
+    if (stopbits == 1 || stopbits == 2)  
+        U2MODEbits.STSEL = stopbits - 1;// number of stopbits
+    else
+        U2MODEbits.STSEL = 0;           // DEFAULT 1 STOPBIT
+
+    U2MODEbits.BRGH = 0;                // Divide by 16
+    U2STAbits.URXEN = 1;                // Enable Receiver
+    U2STAbits.UTXEN = 1;                // Enable Transmitter
+    U2MODEbits.ON = 1;                  // Enable UART2
+}
