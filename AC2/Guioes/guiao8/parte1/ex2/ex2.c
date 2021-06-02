38cf20ac8a35cd25a2e852bc93eff09a63db22d7
AndreClerigo Laptop <andreclerigo@outlook.com>
2021-06-02 14:48:57 +0100

    ex2 guiao8 working

---
+++ AC2/Guioes/guiao8/parte1/ex2/ex2.c
@@ -0,0 +1,39 @@
+#include <detpic32.h>
+
+void delay(int ms);
+void putc(char byte2send);
+
+int main(void)
+{
+    // Configure UART2
+    U2BRG = 10;                 // U2BRG = (20Mhz / (16*115200))-1 ~ 10          
+    U2MODEbits.PDSEL = 0b00;    // 00 = 8-bit data, no parity
+    U2MODEbits.STSEL = 0;       // only 1 stop bit
+    U2MODEbits.BRGH = 0;        // divide by 16
+    U2STAbits.URXEN = 1;        // Enable Receiver
+    U2STAbits.UTXEN = 1;        // Enable Transmitter
+    U2MODEbits.ON = 1;          // Enable UART2
+    
+    while (1)
+    {
+        putc('+');
+        delay(1000);            // wait 1s
+    }
+    
+    return 0;
+}
+
+void putc(char byte2send)
+{
+    while (U2STAbits.UTXBF == 1);
+    U2TXREG = byte2send;
+}
+
+void delay(int ms)
+{   
+    for (; ms > 0; ms--)
+    {
+        resetCoreTimer();
+        while (readCoreTimer() < 20000);
+    }
+}
