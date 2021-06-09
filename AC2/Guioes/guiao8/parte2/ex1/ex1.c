466836eb3e6ac2672cc4e3a3981d41eb6c58a53d
AndreClerigo Laptop <andreclerigo@outlook.com>
2021-06-09 14:30:47 +0100

    ex1 parte2 working

---
+++ AC2/Guioes/guiao8/parte2/ex1/ex1.c
@@ -0,0 +1,60 @@
+#include <detpic32.h>
+
+void putc(char byte2send);
+void configUart(unsigned int baud, char party, unsigned int stopbits);
+
+int main(void)
+{
+    configUart(115200, 'N', 1);             // default pterm parameters
+    EnableInterrupts();                     // with RX interrupt enabled
+    while (1);
+
+    return 0;
+}
+
+
+void putc(char byte2send)
+{
+    while (U2STAbits.UTXBF == 1);
+    U2TXREG = byte2send;
+}
+
+void _int_(32) isr_uart2(void)
+{
+    putc(U2RXREG);
+    IFS1bits.U2RXIF = 0;
+}
+
+void configUart(unsigned int baud, char parity, unsigned int stopbits)
+{
+    // Configure UART2
+    if (baud >= 600 && baud <= 115200)
+        U2BRG = ((PBCLK + 8 * baud) / (16 * baud)) - 1;    // Aproximate to closest baudrate
+    else
+        U2BRG = 10;                     // DEFAULT 115200 baudrate
+    
+    if (parity == 'N')
+        U2MODEbits.PDSEL = 0b00;        // 00 = 8-bit data, no parity
+    else if (parity == 'E')
+        U2MODEbits.PDSEL = 0b01;        // 01 = 8-bit data, even parity
+    else if (parity == 'O')
+        U2MODEbits.PDSEL = 0b10;        // 10 = 8-bit data, odd parity
+    else
+        U2MODEbits.PDSEL = 0b00;        // DEFAULT NO PARITY 8 BITS
+
+    if (stopbits == 1 || stopbits == 2)  
+        U2MODEbits.STSEL = stopbits - 1;// Number of stopbits
+    else
+        U2MODEbits.STSEL = 0;           // DEFAULT 1 STOPBIT
+
+    U2MODEbits.BRGH = 0;                // Divide by 16
+    U2STAbits.URXEN = 1;                // Enable Receiver
+    U2STAbits.UTXEN = 1;                // Enable Transmitter
+
+    U2STAbits.URXISEL = 0b00;           // Select interrupt only for receiver
+    IPC8bits.U2IP = 1;                  // UART2 Priority
+    IEC1bits.U2RXIE = 1;                // U2RX Interrupt Enable
+    IFS1bits.U2RXIF = 0;                // U2RX Interrupt Flag
+
+    U2MODEbits.ON = 1;                  // Enable UART2
+}
