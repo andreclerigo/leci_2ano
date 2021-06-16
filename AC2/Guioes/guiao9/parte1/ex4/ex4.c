e859fe16ff5c3770c658e2687cafe8d7f0ee8cff
AndreClerigo Laptop <andreclerigo@outlook.com>
2021-06-16 14:40:34 +0100

    ex4 done

---
+++ AC2/Guioes/guiao9/parte1/ex4/ex4.c
@@ -0,0 +1,19 @@
+#define BUF_SIZE 8
+
+#include "../../uart.h"
+#include "../../buffer.h"
+#include "detpic32.h"
+
+extern volatile circularBuffer rxb;
+extern volatile circularBuffer txb;
+
+void comDrv_putc(char ch)
+{
+    while (txb.count == BUF_SIZE);          // Wait while buffes is full
+    txb.data[txb.tail] = ch;                // Copy character to TX buffer at 
+                                            // last position avaible (tail)
+    txb.tail = (txb.tail + 1) & INDEX_MASK; // Increment tail index
+    DisableUart2RxInterrupt();              // Begin of critical section
+    txb.count++;                            // Increment count variable
+    EnableUart2TxInterrupt();               // End of critical section
+}
