5ba15918ad159004f204fc145e702ab2424af89d
AndreClerigo Laptop <andreclerigo@outlook.com>
2021-06-16 15:15:01 +0100

    ex6 done

---
+++ AC2/Guioes/guiao9/parte1/ex6/ex6.c
@@ -0,0 +1,23 @@
+#define BUF_SIZE 8
+
+#include "../../uart.h"
+#include "../../buffer.h"
+#include "detpic32.h"
+
+extern volatile circularBuffer txb;
+
+void _int_(32) isr_uart2tx()
+{
+    if (IFS1bits.U2TXIF == 1)
+    {
+        if(txb.count > 0)
+        {
+            U2TXREG = txb.data[txb.head];
+            txb.head = (txb.head + 1) & INDEX_MASK;
+            txb.count--;
+        }
+    }
+
+    if (txb.count == 0) DisableUart2RxInterrupt();
+    IFS1bits.U2TXIF = 0;
+}
--- AC2/Guioes/guiao9/uart.h
+++ AC2/Guioes/guiao9/uart.h
@@ -1,20 +1,10 @@
-#ifndef DisableUart2RxInterrupt()
 #define DisableUart2RxInterrupt() IEC1bits.U2RXIE = 0
-#endif
-
-#ifndef EnableUart2RxInterrupt()
 #define EnableUart2RxInterrupt() IEC1bits.U2RXIE = 2
-#endif
-
-#ifndef Disablet2TxInterrupt()
 #define DisableUart2TxInterrupt() IEC1bits.U2TXIE = 0
-#endif
-
-#ifndef EnableUart2TxInterrupt()
 #define EnableUart2TxInterrupt() IEC1bits.U2TXIE = 1
-#endif
 
-void comDrv_flushRX(void);
+void comDrv_flushRx(void);
 void comDrv_flushTx(void);
 void comDrv_putc(char ch);
 void comDrv_puts(char *s);
+void comDrv_config(unsigned int baud, char party, unsigned int stopbits);
